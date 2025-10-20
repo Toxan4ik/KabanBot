e08ba6b709c120b1f2b7fb04c3544e7507365460
Toxan4ik <108747572+Toxan4ik@users.noreply.github.com>
2025-10-20 16:57:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.filters import Command
+import asyncio
+
+bot = Bot(token="7886069810:AAFgcFNP_1PbM4cfKIkIZnniMnBWHvOEoEc")
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer("Привет")
+
+async def main():
+    await dp.start_polling(bot)
+
+#if __name__ == "__main__":
+    #asyncio.run(main())
