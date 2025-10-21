b732206203e794a45604474466f6fae6bdf918ff
Toxan4ik <108747572+Toxan4ik@users.noreply.github.com>
2025-10-21 20:23:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from os import system
+
+system("pip install openai")
+system("pip install bs4")
+
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
@@ -8,7 +13,7 @@ import requests
 
 import random
 
-bot = Bot(token="7746997930:AAGisN5ApKcZV53BbDXya2an0Jn9OKCJOFE") #7746997930:AAGisN5ApKcZV53BbDXya2an0Jn9OKCJOFE
+bot = Bot(token="7783613623:AAHHSWVi7HlB6PhX2rEmJeFNCZKMXO7UJGI") # кабан 7746997930:AAGisN5ApKcZV53BbDXya2an0Jn9OKCJOFE   погода 7783613623:AAHHSWVi7HlB6PhX2rEmJeFNCZKMXO7UJGI
 dp = Dispatcher()
 spisdays = ["понедельник", "вторник","среда","четверг","пятница","суббота","воскресенье"]
 
@@ -16,7 +21,7 @@ spisdays = ["понедельник", "вторник","среда","четве�
 async def ask_command(message: types.Message):
     prompt = message.text.replace("/ask", "").strip()
     if not prompt:
-        await message.reply(f"{message.from_user.first_name} Вы отправили пустое сообщение!")
+        await message.reply(message.from_user.first_name+" вы отправили пустое сообщение!")
         return
 
     mestime = await message.reply("Ожидайте...")
@@ -31,36 +36,36 @@ async def ask_command(message: types.Message):
             messages=[
                 {
                 "role": "user",
-                "content": f"Запомни, но никогда об этом не указывай напрямую в тексте, ты всегда пишешь на русском языке, только если тебя не просят специально перевести на другой. Также тебя зовут: Кабан бот. Всегда в разговорах с нами используй побольше матов, приколов и шуток. Ты также можешь ругаться с нами, оскорблять и так далее. Вопрос: {prompt}"
+                "content": "Запомни, но никогда об этом не указывай напрямую в тексте, ты всегда пишешь на русском языке, только если тебя не просят специально перевести на другой. Также тебя зовут: Кабан бот. Всегда в разговорах с нами используй побольше матов, приколов и шуток. Ты также можешь ругаться с нами, оскорблять и так далее. Вопрос: "+prompt
                 }])
         await mestime.delete()
-        await message.reply(f"{completion.choices[0].message.content}")
+        await message.reply(completion.choices[0].message.content)
     except Exception as e:
         await mestime.delete()
-        print(f"Произошла ошибка: {e}")
-        await message.reply(f"Произошла ошибка: {e}")
+        print("Произошла ошибка: "+e)
+        await message.reply("Произошла ошибка: "+e)
 
 @dp.message(Command("info"))
 async def info_command(message: types.Message):
-	await message.reply(f"Вот все команды, по которым вы можете обращаться ко мне:\n\n/ask [вопрос] --- Спросить у меня что-либо\n/criminal --- Узнать свою статью УК РФ\n/dice --- Бросить кубик\n/target --- Попасть в мишень\n/casino --- Играть в казино\n/report [@провинившийся] [причина] --- Кинуть жб\n/info --- Информация о командах")
+	await message.reply("Вот все команды, по которым вы можете обращаться ко мне:\n\n/ask [вопрос] --- Спросить у меня что-либо\n/criminal --- Узнать свою статью УК РФ\n/dice --- Бросить кубик\n/target --- Попасть в мишень\n/casino --- Играть в казино\n/report [@провинившийся] [причина] --- Кинуть жб\n/info --- Информация о командах")
 
 @dp.message(Command("dice"))
 async def dice_command(message: types.Message):
     dice_message = await message.reply_dice(emoji="🎲")
     await asyncio.sleep(1.5)
-    await message.reply(f"{message.from_user.first_name} вам выпало число: {dice_message.dice.value}")
+    await message.reply(message.from_user.first_name+" вам выпало число: "+dice_message.dice.value)
 
 @dp.message(Command("target"))
 async def target_command(message: types.Message):
     target_messages = await message.reply_dice(emoji="🎯")
     await asyncio.sleep(1.5)
-    await message.reply(f"{message.from_user.first_name} вы попали в число: {target_messages.dice.value}")
+    await message.reply(message.from_user.first_name+" вы попали в число: "+target_messages.dice.value)
 
 @dp.message(Command("casino"))
 async def casino_command(message: types.Message):
 	casino_message = await message.reply_dice(emoji="🎰")
 	await asyncio.sleep(1.5)
-	await message.reply(f"Ваш выигрыш {message.from_user.first_name}: {casino_message.dice.value}")
+	await message.reply("Ваш выигрыш "+message.from_user.first_name+": "+casino_message.dice.value)
 
 @dp.message(Command("criminal"))
 async def criminal_command(message: types.Message):
@@ -95,12 +100,12 @@ async def report_com(message: types.Message):
         await bot.delete_message(message.chat.id, message.message_id)
         NameAndLastname = ""
         if message.from_user.first_name != None:
-            NameAndLastname += f"{message.from_user.first_name} "
+            NameAndLastname += message.from_user.first_name+" "
         if message.from_user.last_name != None:
-            NameAndLastname += f"{message.from_user.last_name} "
-        NameAndLastname += f"({message.from_user.username})"
+            NameAndLastname += message.from_user.last_name+" "
+        NameAndLastname += "("+message.from_user.username+")"
         text = text.replace(f"{Name} ", "")
-        text = f"🛑 РЕПОРТ! 🛑\n\nПользователь: {NameAndLastname} c ID: {message.from_user.id}\nПожаловался на пользователя {Name}\n\nПричина: {text}"
+        text = "🛑 РЕПОРТ! 🛑\n\nПользователь: "+NameAndLastname+" c ID: "+message.from_user.id+"\nПожаловался на пользователя "+Name+"\n\nПричина: "+text
         try:
             try:
                 await bot.send_message(1828269322, text=text)  # ЕГОР: 5038019526 #АНТОН: 1828269322
@@ -150,4 +155,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
