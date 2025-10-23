832d291163ea54766583093d6f303c1ae13f2a1e
Toxan4ik <108747572+Toxan4ik@users.noreply.github.com>
2025-10-23 23:34:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,11 +47,14 @@ async def reestr(message: types.Message):
         now = datetime.datetime.now()
         ban_until = now + timedelta(minutes=sumbantime)
         timestamp = int(ban_until.timestamp())
-        await bot.restrict_chat_member(
-            chat_id=message.chat.id,
-            user_id=message.from_user.id,
-            permissions=types.ChatPermissions(),
-            until_date=timestamp)
+		try:
+	        await bot.restrict_chat_member(
+	            chat_id=message.chat.id,
+	            user_id=message.from_user.id,
+	            permissions=types.ChatPermissions(),
+	            until_date=timestamp)
+		except:
+			pass
 
 @dp.message(Command("ask"))
 async def ask_command(message: types.Message):
@@ -203,4 +206,5 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
